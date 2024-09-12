f7f6eba326d06e436212bdb2e6362c4906b19e7b
katzhukk <katzhukova230@gmail.com>
2024-09-12 22:49:15 +0300

    reload StudentRegForm.java

--- src/test/java/StudentRegForm.java
+++ src/test/java/StudentRegForm.java
@@ -10,10 +10,10 @@ public class StudentRegForm {
 
     @BeforeAll
     static void beforeAll() {/* Всегда пишется со static. Вызывается один перед всеми тестами в этом тестовом классе*/
-        Configuration.browserSize = "1920x1080";    /* Задаем разрешение браузера */
+        Configuration.browserSize = "1920x1080";        /* Задаем разрешение браузера */
         Configuration.baseUrl = "https://demoqa.com";   /* Открывает основную страницу сайта */
-        Configuration.pageLoadStrategy = "eager";   /* Не ждем, когда загрузится полностью страница, чтобы долго не ждать*/
-        //Configuration.holdBrowserOpen = true; /* не дает закрыть тесту браузер */
+        Configuration.pageLoadStrategy = "eager";       /* Не ждем, когда загрузится полностью страница, чтобы долго не ждать*/
+        //Configuration.holdBrowserOpen = true;         /* Не дает закрыть тесту браузер. Нужно только для отладки */
     }
 
     @Test
@@ -28,39 +28,39 @@ public class StudentRegForm {
         String State = "NCR";
         String city = "Delhi";
 
-        open("/automation-practice-form");    /*Открывает форму automation-practice-form*/
+        open("/automation-practice-form"); /*Открывает форму automation-practice-form*/
         /*Заполнение формы студента:*/
-        $("#firstName").setValue(firstName);    /*Вводит в форму firstName. Можно сократить: для id= - #, для класса "точка" */
-        $("#lastName").setValue(lastName);    /*Вводит в форму lastName. Можно сократить: для id= - #, для класса "точка" */
-        $("#userEmail").setValue(userEmail);    /*Вводит в форму Email*/
+        $("#firstName").setValue(firstName);      /*Вводит в форму firstName. Можно сократить: для id= - #, для класса "точка"*/
+        $("#lastName").setValue(lastName);        /*Вводит в форму lastName. Можно сократить: для id= - #, для класса "точка"*/
+        $("#userEmail").setValue(userEmail);      /*Вводит в форму Email*/
         $("#gender-radio-2").parent().click();    /*Выбор пола по клику*/
         $("#userNumber").setValue(userNumber);    /*Вводит в форму номер телефона */
 
         $("#dateOfBirthInput").click();    /*Клик по Дате Рождения*/
-        $(".react-datepicker__month-select").find("option[value='5']").click(); /*Выбирает июнь*/
-        $(".react-datepicker__year-select").find("option[value='2000']").click();
-        $(".react-datepicker__day--023").click();
+        $(".react-datepicker__month-select").find("option[value='5']").click();     /*Выбирает июнь*/
+        $(".react-datepicker__year-select").find("option[value='2000']").click();   /*Выбирает год*/
+        $(".react-datepicker__day--023").click();                                      /*Выбирает день*/
 
         $("#subjectsInput").setValue(subjectsInput).pressEnter();    /*Вводит в форму предметы*/
-        $("#hobbies-checkbox-3").parent().click();     /*Выбор хобби Music по клику*/
+        $("#hobbies-checkbox-3").parent().click();                   /*Выбор хобби Music по клику*/
         $("#uploadPicture").uploadFromClasspath("images/StudentRegForm.jpg");     /*Выбор картинку по клику*/
-        $("#currentAddress").setValue(currentAddress);    /*Вводит в форму предметы*/
-        $("#react-select-3-input").setValue(State).pressEnter();    /*Вводит в форму штат*/
-        $("#react-select-4-input").setValue(city).pressEnter();    /*Вводит в форму город*/
+        $("#currentAddress").setValue(currentAddress);                /*Вводит в форму предметы*/
+        $("#react-select-3-input").setValue(State).pressEnter();      /*Вводит в форму штат*/
+        $("#react-select-4-input").setValue(city).pressEnter();       /*Вводит в форму город*/
 
         $("#submit").click();    /*Клик по кнопке submit*/
 
         /*Проверка параметров:*/
-        $(".table-responsive").shouldHave(text(firstName + " " + lastName)); /*Проверяет имя и фамилию*/
-        $(".table-responsive").shouldHave(text(userEmail)); /*Проверяет Email*/
-        $(".table-responsive").shouldHave(text("Female")); /*Проверяет пол*/
-        $(".table-responsive").shouldHave(text(userNumber)); /*Проверяет номер*/
-        $(".table-responsive").shouldHave(text(dateOfBirthInput)); /*Проверяет День Рождения*/
-        $(".table-responsive").shouldHave(text(subjectsInput)); /*Проверяет предметы*/
-        $(".table-responsive").shouldHave(text("Music")); /*Проверяет предметы*/
-        $(".table-responsive").shouldHave(text("StudentRegForm.jpg")); /*Проверяет картинку*/
-        $(".table-responsive").shouldHave(text(currentAddress)); /*Проверяет адрес*/
-        $(".table-responsive").shouldHave(text(State + " " + city)); /*Проверяет штат и город*/
+        $(".table-responsive").shouldHave(text(firstName + " " + lastName));/*Проверяет имя и фамилию*/
+        $(".table-responsive").shouldHave(text(userEmail));                 /*Проверяет Email*/
+        $(".table-responsive").shouldHave(text("Female"));                  /*Проверяет пол*/
+        $(".table-responsive").shouldHave(text(userNumber));                /*Проверяет номер*/
+        $(".table-responsive").shouldHave(text(dateOfBirthInput));          /*Проверяет День Рождения*/
+        $(".table-responsive").shouldHave(text(subjectsInput));             /*Проверяет предметы*/
+        $(".table-responsive").shouldHave(text("Music"));                   /*Проверяет предметы*/
+        $(".table-responsive").shouldHave(text("StudentRegForm.jpg"));      /*Проверяет картинку*/
+        $(".table-responsive").shouldHave(text(currentAddress));            /*Проверяет адрес*/
+        $(".table-responsive").shouldHave(text(State + " " + city));        /*Проверяет штат и город*/
     }
     @AfterAll
     static void AfterAll() {/* Всегда пишется со static. Вызывается один после всеми тестов в этом тестовом классе*/
